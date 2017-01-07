000a15d7787ef779355d0fe4f5e4c958a95d263f
ytyaru <ytyaru@outlook.jp>
2017-01-07 10:42:54 +0900

    check LIKE select.

--- res/sql/check/check.sql
+++ res/sql/check/check.sql
@@ -4,3 +4,4 @@ select * from sqlite_master where type = 'table';
 select * from Accounts;
 select * from TwoFactors;
 select * from AccessTokens;
+select * from AccessTokens where (',' || Scopes || ',') LIKE '%,repo,%';
