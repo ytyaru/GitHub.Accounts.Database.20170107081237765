5b351a0ae2564172bfb133f28f22944546d373d2
ytyaru <ytyaru@outlook.jp>
2017-01-09 12:35:09 +0900

    add IdOnGitHub field.

--- res/sql/create/AccessTokens.sql
+++ res/sql/create/AccessTokens.sql
@@ -1,6 +1,8 @@
 create table AccessTokens(
     Id              integer primary key,
     AccountId       integer not null,
+    IdOnGitHub      integer not null,
+    Note            text,
     AccessToken     text not null,
     Scopes          text,
     foreign key(AccountId) references Accounts(Id)
