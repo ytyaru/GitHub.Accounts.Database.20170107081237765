d6935adf99dd0674f50992b2baa70a29f810f164
ytyaru <ytyaru@outlook.jp>
2017-01-09 13:09:13 +0900

    add unique constraint to IdOnGitHub field.

--- res/sql/create/AccessTokens.sql
+++ res/sql/create/AccessTokens.sql
@@ -1,7 +1,7 @@
 create table AccessTokens(
     Id              integer primary key,
     AccountId       integer not null,
-    IdOnGitHub      integer not null,
+    IdOnGitHub      integer unique not null,
     Note            text,
     AccessToken     text not null,
     Scopes          text,
